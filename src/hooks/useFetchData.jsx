e94b1778b659455c3f19443128ece596ac11e0ae
kzsharma <kzsharma@daxsws.com>
2024-09-10 11:26:01 +0530

    ind

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {useState,useEffect} from 'react'
 export default function useFetchData() {
-    const [list, setList] = useState([]);
+  const [list, setList] = useState([]);
   useEffect(() => {
     axios.get("https://www.aonflow.com/blog/wp-json/wp/v2/posts")
     .then((res) => setList(res.data));
