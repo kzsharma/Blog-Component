e8da03c02074f459dcf158322a3cfd3f717eaaef
kzsharma <kzsharma@daxsws.com>
2024-09-10 11:24:11 +0530

    indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 function App() {
   return (
-   <>
+    <>
       <div className='app'>
-      <div className='look'>
-      <Card />
+        <Card />
       </div>
-      </div>
-   </>
+    </>
   )
 }
 
--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import {React,useState,useEffect} from 'react'
-
+import {useState,useEffect} from 'react'
 export default function useFetchData() {
     const [list, setList] = useState([]);
   useEffect(() => {
     axios.get("https://www.aonflow.com/blog/wp-json/wp/v2/posts")
-      .then((res) => setList(res.data));
+    .then((res) => setList(res.data));
   }, [list]);
   return list
 }
