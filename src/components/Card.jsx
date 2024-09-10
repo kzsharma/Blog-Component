a4de41565530264ddc36e01d57e0f6b9ef9ab9d0
kzsharma <kzsharma@daxsws.com>
2024-09-10 12:28:09 +0530

    indentaion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,21 +7,28 @@ function Card() {
     const Array = list.slice(0, 3)
     return (
         <Splide
-            options={{type: 'loop',perPage: 1}} tag="section">{Array.map((name, index) => (
-                <SplideSlide key={index}><div>
-                    <a href={name.link}>
-                        <div className='card'>
-                            <div className='image'>
-                                <img src={name.uagb_featured_image_src.full[0]} />
+            options={{
+                type: 'loop',
+                perPage: 1
+            }}
+            tag="section">
+            {Array.map((name, index) => (
+                <SplideSlide key={index}>
+                    <div>
+                        <a href={name.link}>
+                            <div className='card'>
+                                <div className='image'>
+                                    <img src={name.uagb_featured_image_src.full[0]} />
+                                </div>
+                                <div className='card-data'>
+                                    <p>
+                                        {name.title.rendered}
+                                    </p>
+                                </div>
                             </div>
-                            <div className='card-data'>
-                                <p>
-                                    {name.title.rendered}
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                </div></SplideSlide>
+                        </a>
+                    </div>
+                </SplideSlide>
             ))}
 
         </Splide>
