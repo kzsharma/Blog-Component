1c3ac8e71780211f9edbd7ea2d889a8031f8af37
kzsharma <kzsharma@daxsws.com>
2024-09-13 10:52:58 +0530

    MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Blog from './components/Blog'
-import Card from './components/Card'
+
 function App() {
   return (
     <>
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -5,58 +5,89 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
 function Blog() {
     const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('https://www.aonflow.com/blog/wp-json/wp/v2/posts');
-                setList(response.data);
+                setList(response.data.slice(0,3));
+                setIsLoading(false)
             } catch (error) {
             }
         };
         fetchData();
     }, []);
     return (
-        <Swiper
-            slidesPerView={1}
-            spaceBetween={30}
-            centeredSlides={true}
-            loop={true}
-            autoplay={{
-                delay: 1500,
-                disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-        >
-            {list.map((name, index) => {
-                return (
-                    <>
-                        <SwiperSlide key={index}>
-                            <div>
+        <>{isLoading ? <>
+            <Stack height={'100vh'}>
+                <Box sx={{
+                    height: '100%',
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    breakpoints: { xl: 300 }
+                }}
+                >
+                    <CircularProgress thickness={5} size={100} />
+                </Box>
+            </Stack></>
+            : <>
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    loop={true}
+                    autoplay={{
+                        delay: 1500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                >
+                    {list.map((name, index) => {
+                        return (
+                            <>
+                                <SwiperSlide key={index}>
                                 <a href={name.link}>
-                                    <div className='card'>
-                                        <div className='image'>
-                                            <img alt='Blog-Image' src={name.uagb_featured_image_src.full[0]} />
-                                        </div>
-                                        <div className='card-data'>
-                                            <p>
-                                                {name.title.rendered}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </SwiperSlide>
-                     </>
-                )
-            })}
-        </Swiper>
-
+                                    <Card>
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                image={name.uagb_featured_image_src.full[0]}
+                                                alt="Blog Image"
+                                            />
+                                            <CardContent>
+                                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                                    {name.title.rendered}
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+                                    </Card>
+                                    </a>
+                                </SwiperSlide>
+                            </>
+                        )
+                    })}
+                </Swiper>
+            </>
+        }
+        </>
     )
 
 }
